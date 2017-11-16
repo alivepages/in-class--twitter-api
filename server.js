b318bae9c75687ade519e2bd67b7dfc7557a4ce2
Joel <joel.fagundo@gmail.com>
2017-11-16 16:29:46 -0600

    404 page

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const fs = require('fs-extra');
+const ejs = require('ejs');
 
 const pageRouter = require('./src/routes/pageRouter.js');
 const apiRouter = require('./src/routes/apiRouter.js');
 
 const app = express();
 
+// CONFIGURE ejs
+app.engine('ejs', ejs.renderFile);
+app.set('views engine', 'ejs');
+app.set('views', `${__dirname}/src/views`);
+
 const PATH = `${__dirname}/src/views/home.html`;
 
 app.use('/', pageRouter);
 app.use('/api/v1', apiRouter);
+app.use(express.static(__dirname + '/public'));
 
 // Create 404 route
 app.use((req, res) => {
-  res.send('<h1>404. Not found.</h1>')
+  res.render('404.ejs');
 });
-
-// app.use('/', (req, res) => {
-//   fs
-//     .readFile(PATH, 'utf-8')
-//     .then(data => {
-//       res.send(data);
-//     })
-//   })
 
 const PORT = process.env.PORT || 3000;
 
